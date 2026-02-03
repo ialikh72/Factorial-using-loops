eeeeb741b517e4a6023f06291b11466b6993918d
ialikh72 <ialikh.w1@gmail.com>
2026-02-03 21:55:17 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Fac
+{
+    static void Main()
+    {
+       int fact= Factorial();
+        Console.WriteLine("F="+fact);
+    }
+    static int Factorial()
+    {
+        Console.WriteLine("enter namber for factorial");
+        int num = int.Parse(Console.ReadLine());
+        int i = 1, fact = 1;
+        while (i <= num)
+        {
+            fact = fact * i;
+            i++;
+        }
+       
+        return fact;
+    }
+}
